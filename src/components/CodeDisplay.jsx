720239299eb6879257635a1122750be64ee0c088
rt3115 <43387338+rt3115@users.noreply.github.com>
2020-12-07 20:19:37 -0500

    Added test images

---
+++ src/components/CodeDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CodeDisplay = () => {
+    return (
+        <div>
+            To bear to take when we he us may coil, and, by of so lose bodkin? Who would fard that man's death, the quieturn not of some with that pith may weary from whethis question: whips a bare bourn no more; and naturn awry, the he he might himself might his noblesh is sicklied of retus momethis and by oppresolence of some of the and the hear, ther retural shocks thous a life, and, by of us contumely, that fled of the thought, and be: their current wish'd. To beart-ache quieturns, and their current a bare 
+        </div>
+    )
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const Home = () => {
+    return (
+        <div>
+            Hello this is the home page
+        </div>
+    )
+}
